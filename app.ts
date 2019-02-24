7c36a3dce09b467a205e322322ecff4dca5cd628
Cerci <lucas.fernandes001p@gmail.com>
2019-02-24 11:22:22 -0300

    function example

--- app.ts
+++ app.ts
@@ -22,3 +22,9 @@ console.log(`Is ${distance} parsecs enough to beat Millennium Falcon? ${isEnough
 let call = (name: string) => console.log(`Do you copy, ${name}? `)
 call('R2')
 
+function inc (speed: number, inc: number = 1) : number {
+	return speed + inc
+}
+
+console.log(`inc (5, 1) = ${inc(5, 1)}`)
+console.log(`inc (5) = ${inc(5)}`)
